d443e3a1bb0b16a83869a460c8bd65cf5669e2b0
felipeandres92 <Felipe@MacBook-Air-de-Felipe.local>
2015-06-12 01:34:07 -0300

    Danmau

--- models/user.js
+++ models/user.js
@@ -1,43 +1,32 @@
-'use strict'
+'use strict';
 
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     bcrypt = require('bcrypt'),
     SALT_WORK_FACTOR = 10;
- 
+
 var UserSchema = new Schema({
     username: { type: String, required: true, index: { unique: true } },
-    user: { type: String, required: true }
+    password: { type: String, required: true }
 });
 
-UserSchema.pre(save, function(next) {
+UserSchema.pre('save', function(next) {
     var user = this;
- 
-// only hash the user if it has been modified (or is new)
-if (!user.isModified('user')) return next();
- 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
- 
-    // hash the user using our new salt
-    bcrypt.hash(user.user, salt, function(err, hash) {
-        if (err) return next(err);
- 
-        // override the cleartext user with the hashed one
-        user.user = hash;
-        next();
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(error, salt) {
+        if (error) return next(error);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(error, hash) {
+            if (error) return next(error);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
     });
 });
- 
- 
-});
- 
-UserSchema.methods.compareUser = function(candidateUser, cb) {
-    bcrypt.compare(candidateUser, this.user, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
- 
-module.exports = mongoose.model('User', UserSchema);
